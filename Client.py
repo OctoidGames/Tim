3a3b104491870761d94449287bd47581b3c2f643
OctoidGames <87039261+OctoidGames@users.noreply.github.com>
2021-07-07 02:36:59 +0000

    added original files
    watched this tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
    basically copied their script but added correct formatting when sending a reply to a client

---
+++ Client.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+import time
+
+
+HEADER = 64
+PORT = 14677
+SERVER = "127.0.1.1"
+ADDR = (SERVER,PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "UwU I'm sowwy, buwt I gowtta go now~~"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message=msg.encode(FORMAT) # encodes message to bytes
+    msg_length = len(message) # finds length
+    send_length = str(msg_length).encode(FORMAT) # encodes length
+    send_length += b" " * (HEADER-len(send_length)) # pads length to fill HEADER
+    client.send(send_length) #sends length
+    client.send(message) # sends message
+    print(f"[MESSAGE SENT] {msg}")
+    #print("[LISTENING] client listening")
+    msg_length = client.recv(HEADER).decode(
+        FORMAT)  # Waits for message from client telling how long the next message will be (must be length 64 bytes)
+    if msg_length:
+        msg_length = int(msg_length)  # Converts received message length to usable format
+        msg = client.recv(msg_length).decode(FORMAT)  # Waits for message
+        print(f"[MESSAGE] '{msg}'")
+
+send("Hello World")
+
+send(DISCONNECT_MESSAGE)
+
+while(1==1):
+    pass
---
+++ server.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+
+
+HEADER = 64
+PORT = 14677
+SERVER = socket.gethostbyname(socket.gethostname()) # gets local IP of machine
+ADDR = (SERVER,PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "UwU I'm sowwy, buwt I gowtta go now~~"
+
+
+server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def send(conn, addr, msg):
+    message=msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER-len(send_length))
+    conn.send(send_length)
+    conn.send(message)
+    print(f"[MESSAGE SENT] to {addr}: '{msg}'")
+
+
+def handle_client(conn, addr):
+    print("[CONNECTION STARTED] connection with",addr,"started successfully")
+    connected=True
+    while(connected): # Loops while connected to server
+        msg_length = conn.recv(HEADER).decode(FORMAT) # Waits for message from client telling how long the next message will be (must be length 64 bytes)
+        if msg_length:
+            msg_length = int(msg_length) # Converts received message length to usable format
+            msg = conn.recv(msg_length).decode(FORMAT) # Waits for message
+            if msg == DISCONNECT_MESSAGE: # Checks for disconnect message
+                connected = False # Closes loop
+            print(f"[MESSAGE] from @{addr}: '{msg}'")
+            REPLY = "message recieved"
+            send(conn, addr, REPLY ) # sends conformation
+    conn.close() # closes connection
+    print("[INFO] Active conncetions:", threading.activeCount() - 2)
+
+
+def start(): # handles new connections
+    server.listen() # sets the sever to listen for new connections
+    while(True):
+        conn, addr = server.accept() # waits for new connection and sets conn to socket object of connection
+        print("[NEW CONNECTION] @", addr)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))  # Creates new thread for connection
+        thread.start()                                                      # Starts new thread
+        print("[INFO] Active conncetions:", threading.activeCount()-1)
+
+
+print("[STARTING] server is starting at",ADDR)
+start()
